f8c72cf6378cdad7895aaba408faa5afa6ffed7c
dudu9999 <ecaetanocorrea@gmail.com>
2020-01-14 16:50:44 -0300

    Updates

---
+++ precache-manifest.86f4d6de13ca4de7f8eac3fbbcca4081.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a47235bb715c3bb20ed599406a604cf",
+    "url": "/portifolio/index.html"
+  },
+  {
+    "revision": "a3ea9ce16aa36f6eb74c",
+    "url": "/portifolio/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "0608093346d9a9b4fcb4",
+    "url": "/portifolio/static/js/2.1d5a028e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portifolio/static/js/2.1d5a028e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a3ea9ce16aa36f6eb74c",
+    "url": "/portifolio/static/js/main.d2936346.chunk.js"
+  },
+  {
+    "revision": "a17fc5db057aebf325ff",
+    "url": "/portifolio/static/js/runtime-main.c4358f58.js"
+  }
+]);
